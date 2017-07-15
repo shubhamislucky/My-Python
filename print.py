b87a5af234cd784d4e5d46c79d118fc8e7cc2556
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-07-15 14:51:07 +0530

    added print.py

---
+++ print.py
@@ -0,0 +1,10 @@
+# in this script we will be revising the print thing
+
+print("hello there how are you")
+print("I really like your tshirt. Where did you buy it?")
+shop = input("> ")
+print("Oh! So you bought it from {}.".format(shop))
+print(f"Well {shop} is a nice shop")
+
+name = f"What is your name?"
+print(name)
