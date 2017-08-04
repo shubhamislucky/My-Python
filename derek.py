5a11acd853ddc6812a2eecf99ba00513c1b36118
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-08-04 15:06:47 +0530

    hello

--- derek.py
+++ derek.py
@@ -4,3 +4,12 @@ mylist.append("deep")
 mylist.insert(1, "Yuvraj")
 print(mylist)
 print(max(mylist)) # whatever is highest alphabetically
+
+mydict = {"first":"shubham","second":"vicky"}
+print(mydict.keys())
+print(mydict.values())
+print(mydict["first"])
+
+hello = "my name is shubham"
+new = hello.strip().split()
+print(new)
