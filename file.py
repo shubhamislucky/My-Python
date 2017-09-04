801aee9170c46ee0adafe73ec12a4f49e4a2d42b
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-09-04 13:49:26 +0530

    added new stuff

--- file.py
+++ file.py
@@ -24,3 +24,30 @@ with open('../../hello.txt') as f:
 with open('/Users/shubhamislucky/Desktop/hello.txt') as f:
     contents = f.read()
     print(contents.rstrip())
+
+# Making a list of lines from a file
+# f.readlines() function returns a list
+with open('test.txt', 'r') as f:
+    contents = f.readlines()
+    for item in contents:
+        print(item.rstrip())
+
+print()
+sayings = ''
+for item in contents:
+    sayings += item.strip()
+
+print(sayings)
+print(len(sayings))
+
+# Working with large files
+filename = 'resources/chapter_10/pi_million_digits.txt'
+
+with open(filename, 'r') as f:
+    lines = f.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
