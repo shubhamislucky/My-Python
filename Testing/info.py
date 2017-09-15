b8d8cacb09c2a5c3eb75f9e06dce73a688455289
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-09-15 15:53:02 +0530

    testing

---
+++ Testing/info.py
@@ -0,0 +1,7 @@
+with open('info.txt','w') as f:
+    f.write("""
+            Now we are going to learn about testing our code.
+            This is going to be real fun.
+            So hold your horses and be prepared for this shit.
+            After this we will make a game in python which will be interesting as fuck.
+    """)
