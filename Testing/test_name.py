369c3c35f4492d8909cfcd08741944b8301179b0
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-09-15 16:15:11 +0530

    testing

--- Testing/test_name.py
+++ Testing/test_name.py
@@ -8,4 +8,8 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('Shubham', 'Singh')
         self.assertEqual(formatted_name,'Shubham Singh')
 
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_name('Shubham', 'kumar', 'singh')
+        self.assertEqual(formatted_name, 'Shubham Kumar Singh')
+
 unittest.main()
