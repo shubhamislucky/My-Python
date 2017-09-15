661d2daba97bb8211a063ffea1c31d7749652d74
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-09-16 01:56:37 +0530

    pygame

---
+++ pygame/info.py
@@ -0,0 +1,9 @@
+with open('info.txt', 'w') as f:
+    f.write("""
+            So,,,,, We are going to make a fucking game in python!
+            Yeah, you heard me right.
+            This game will be about alien invasion.
+            We will have a ship that fires bullets at alien ships.
+            You know the 90 classics? right....well you guessed it.
+            Let's get on with it then.
+    """)
